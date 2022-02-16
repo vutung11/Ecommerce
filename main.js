3c89c09410a273104383b8a71050b2065d909758
vutung11 <vuvantungip7@gmail.com>
2022-02-16 08:23:03 +0700

    final

---
+++ main.js
@@ -0,0 +1,10 @@
+var LoginForm = document.querySelector(".LoginForm");
+var RegForm = document.querySelector(".RegFrom");
+var Indicator = document.querySelector(".Indicator");
+
+function register() {
+  var a = 2;
+  var b = 5;
+  console.log(a + b);
+  LoginForm.style.display = "none";
+}
